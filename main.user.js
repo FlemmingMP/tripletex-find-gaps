301bb00e906793aabf9b3df502a31ab15d6036ab
Flemming Møller Pedersen <flemming@bjerk.io>
2024-03-11 12:58:01 +0100

    init

---
+++ main.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         Find Tripletex Gaps
+// @namespace    https://github.com/FlemmingMP/tripletex-find-gaps
+// @version      0.05
+// @description  Show gaps in time sheet
+// @author       FlemmingMP
+// @updateUrl    https://github.com/FlemmingMP/tripletex-find-gaps/main.user.js
+// @downloadUrl  https://github.com/FlemmingMP/tripletex-find-gaps/main.user.js
+// @match        https://tripletex.no/execute/updateHourlist*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict'
+
+  const siteValues = {
+    weekdayID: '[id^="timeReportTableWeekdayRow"]',
+    commentNum: 7
+  }
+
+  let delayCount = 0
+  let maxDelay = 10
+
+  // Run after site loads since it takes time
+  // Set a limit on retries so it doesn't run forever
+  runDelay()
+
+  function runDelay() {
+    setTimeout(() => {
+      delayCount++
+      main();
+    }, "100");  
+  }
+
+  function main() {
+    if (findDays() === -1 && delayCount < maxDelay) {
+      runDelay()
+    } else if (delayCount >= maxDelay) {
+      console.log(`Tried to access the data ${maxDelay} times without success\nRefresh the site to try again`)
+    }
+  }
+
+  function findDays() {
+    const dayNodes = document.querySelectorAll(siteValues.weekdayID);
+
+    if (dayNodes.length === 0) {
+      return -1
+    }
+
+    dayNodes.forEach((node) => {
+
+      let num = siteValues.commentNum
+      let curNode = node
+      let stringArr = []
+      let startArr = []
+      let endArr = []
+      let dayText = node.innerText
+      let text = ""
+
+      // Find all strings with time
+      while (curNode.nextSibling.nextSibling && curNode.nextSibling.nextSibling.childNodes[num] && node.nextSibling.nextSibling.childNodes[num].innerText !== '') {
+        curNode = curNode.nextSibling.nextSibling
+        curNode.childNodes[num].querySelectorAll("b").forEach(node => stringArr.push(node.innerText))
+      }
+
+      // Sort and organize time strings
+      stringArr = stringArr.map(string => string.trim())
+      startArr = stringArr.filter(string => string.startsWith("(")).sort().slice(1).map(string => string.slice(1))
+      endArr = stringArr.filter(string => !string.startsWith("(")).sort().slice(0, -1).map(string => string.slice(0, -1))
+
+      // Build string
+      for (let index = 0; index < startArr.length; index++) {
+        if (!startArr[index].includes(endArr[index])) {
+          if (startArr[index] > endArr[index]) {
+            text = text + ` - Gap between ${endArr[index]} and ${startArr[index]}`
+          } else if (startArr[index] < endArr[index]) {
+            text = text + ` - Overlap between ${endArr[index]} and ${startArr[index]}`
+          }
+        }
+      }
+
+      // Add string to row
+      node.innerHTML = 
+      `<td colspan="4">
+        <strong>${dayText}</strong>
+        <span>${text}</span>
+      </td>`
+    })
+    return 0
+  }
+})();
